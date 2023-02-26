699d19907bc6e2896a43da4bd61690dab8c78f75
musicode <musicode@qq.com>
2023-02-26 23:03:22 +0800

    bump 0.0.2

--- ios/RNTCodeView/Code/CodeScannerConfiguration.swift
+++ ios/RNTCodeView/Code/CodeScannerConfiguration.swift
@@ -2,46 +2,43 @@
 import UIKit
 import Foundation
 
-let bundlePath = Bundle(for: CodeScannerConfiguration.self).path(forResource: "CodeView", ofType: "bundle")!
-
+let bundlePath = Bundle(for: CodeScannerConfiguration.self).path(forResource: "HeroCodeView", ofType: "bundle")!
 let bundle = Bundle(path: bundlePath)
 
-let budle1 = Bundle(for: CodeScannerConfiguration.self).resourceURL?.appendingPathComponent("/CodeView.bundle")
-
 @objc public class CodeScannerConfiguration: NSObject {
-    
+
     public var viewFinderMaskColor = UIColor.black.withAlphaComponent(0.61)
-    
+
     public var viewFinderCornerColor = UIColor(red: 1, green: 0.48, blue: 0.03, alpha: 1)
-    
+
     public var viewFinderCornerWidth: CGFloat = 2
-    
+
     public var viewFinderCornerSize: CGFloat = 16
-    
+
     public var viewFinderBorderWidth = 1 / UIScreen.main.scale
-    
+
     public var viewFinderBorderColor = UIColor.white.withAlphaComponent(0.6)
-    
+
     public var guideLabelTextFont = UIFont.systemFont(ofSize: 12)
-    
+
     public var guideLabelTextColor = UIColor.white.withAlphaComponent(0.7)
-    
+
     public var guideLabelMarginTop: CGFloat = 14
-    
+
     public var laserGap: CGFloat = 10
-    
+
     public var laserHeight: CGFloat = 1
-    
+
     public var laserColor = UIColor(red: 1, green: 0.48, blue: 0.03, alpha: 0.625)
-    
+
     public var torchOnImage = UIImage(named: "code_scanner_torch_on", in: bundle, compatibleWith: nil)
-    
+
     public var torchOffImage = UIImage(named: "code_scanner_torch_off", in: bundle, compatibleWith: nil)
-    
+
     public var torchButtonWidth: CGFloat = 44
-    
+
     public var torchButtonHeight: CGFloat = 44
-    
+
     public var torchButtonMarginBottom: CGFloat = 0
 
 }
